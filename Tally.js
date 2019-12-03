66ecf72c973eb14396cee1e8dd5fbf70966f54b0
FabianLCH <fabian.lozanoc23@gmail.com>
2019-12-03 15:33:37 -0800

    Add middleware that checks db for current date

--- Tally.js
+++ Tally.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Tally = new Schema({
-    date: String,
+    date: {
+        year: Number,
+        month: Number,
+        day: Number
+    },
     totalEntries: Number,
     moods: [{
         name: String,
--- server.js
+++ server.js
@@ -39,29 +39,39 @@ app.use("/api", router);
 // calculate current date and pass to next middleware 
 router.use("/moods", (req, res, next) => {
     let today = new Date();
-    let dateString = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
+    let dateObject = { year: today.getFullYear(), month: today.getMonth() + 1, day: today.getDate() };
 
-    req.dateString = dateString;
+    req.dateObject = dateObject;
 
     next();
 });
 
-// get mood data for the current date
-router.get("/moods/today", (req, res) => {
+router.use("/moods/today", (req, res, next) => {
 
-    let { dateString } = req;
+    let { dateObject } = req;
+    let formattedDate = `${dateObject.year}-${dateObject.month}-${dateObject.day}`;
 
-    Tally.findOne({date: dateString}, (err, tallyDocument) => {
+    Tally.findOne( { date: dateObject }, (err, tallyDocument) => {
         if(err) 
             return res.json({success: false, error: err, data: []});
 
         // if document for current date does not exist, create a new one
         if(!tallyDocument) {
-            console.log(`No entries found for ${dateString}. Creating entry...`);
+            console.log(`No entries found for ${formattedDate}. Creating entry...`);
 
             let tallyObj = {
-                date: dateString, 
-                moods: moodList.map((entry) => { return {name: entry, tally: 0} }) 
+                date: dateObject, 
+                totalEntries: req.method === "POST" ? 1 : 0,
+                moods: moodList.map((entry) => { 
+                    // for POST request with no existing document
+                    if(req.method === "POST") {
+                        let { mood } = req.body;
+
+                        return { name: entry, tally: entry === mood ? 1 : 0 }
+                    }
+
+                    return {name: entry, tally: 0} 
+                })
             };
 
             // create document using schema
@@ -71,84 +81,67 @@ router.get("/moods/today", (req, res) => {
             todayTallyCount.save((err) => {
                 if (err) {
                     console.log("Could not create new entry.\n", err);
-                    return res.json({success: false, error: `No entry for ${dateString}.`, data: []});
+                    return res.json({success: false, error: `No entry for ${formattedDate}.`, data: []});
                 }
                 else {
-                    console.log(`New entry for ${dateString} has been created.`);
-                    return res.json({success: true, data: tallyObj});   
+                    console.log(`New entry for ${formattedDate} has been created.`);
+                    return req.method === "POST" ? res.status(204).send() : res.json({success: true, data: tallyObj});   
                 }
             });
         }
-        else if(tallyDocument.moods.length < moodList.length)
-        {
-            console.log(`Number of moods being measured for ${dateString} does not match the currently measurable moods. Adding missing entries...`);
-
-            // create a list of moods for dataset and db
-            let datasetMoods = tallyDocument.moods.map( (mood) => mood.name );
-
-            // filter missing moods
-            let missingMoods = moodList.filter( allowedMood => {
-                for(let i=0; i < datasetMoods.length; i++) {
-                    // if an allowed mood is in the dataset, don't include in missing list
-                    if(allowedMood === datasetMoods[i])
-                        return false;
-                }
-                // if it is, include it
-                return true;
-            });
-
-            console.log("Adding the following missing moods:\n");
-
-            let objMappedMissingMoods = missingMoods.map( mood => { return {name: mood, tally: 0} }); 
-            console.log(objMappedMissingMoods);
-
-            Tally.updateOne(
-                { date: dateString }, 
-                { $addToSet: { moods: { $each : objMappedMissingMoods } } }, 
-                (err) => {
-                    if(err) console.log("Could not add missing moods.\n", err);
-                    else console.log("Added missing moods successfully.");
-                    
-                    return res.json({success: true, data: tallyDocument});
-            });
-
-        } // if entry exists, simply return it
-        else return res.json({success: true, data: tallyDocument});
+        // if entry already exists, send to next middleware
+        else {
+            req.tallyDocument = tallyDocument;
+            next();
+        }
     });
 });
 
-// get moods for previous dates
-router.get("/moods/previous", (req, res) => {
-    let { dateString } = req;
+// get mood data for the current date
+router.get("/moods/today", (req, res) => {
 
-    Tally.find({ date: { $not: { $eq: dateString } } }, (err, tallyDocumentList) => {
-        if(err)
-            return res.json({success: false, error: err, data: []});
-        
-        return res.send({success: true, data: tallyDocumentList});
-    });
+    let { tallyDocument } = req;
+    res.json({success: true, data: tallyDocument});
 });
 
 // update tally count for a particular mood 
 router.post("/moods/today", (req, res) => {
-    let today = new Date();
-    let dateString = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
-
+    let { dateObject, tallyDocument } = req;
     let { mood } = req.body;
     
     let coloredMood = "\x1b[35m" + mood + "\x1b[0m";
-    let coloredDate = "\x1b[36m" + dateString + "\x1b[0m";
+    let coloredDate = "\x1b[36m" + `${dateObject.year}-${dateObject.month}-${dateObject.day}` + "\x1b[0m";
+
+    let targetMoodIndex = tallyDocument.moods.findIndex( docMood => docMood.name === mood );
+
+    // check if the specified mood is in the array
+    if( targetMoodIndex === -1 ) {
+        console.log(`No entry for ${coloredMood} in the database.`);
+        return res.status(404).send();
+    }
 
     console.log(`Adding tally to ${coloredMood} for ${coloredDate}...`)
 
-    Tally.findOneAndUpdate(
-        { date: dateString, "moods.name": mood }, 
-        { $inc: { "moods.$.tally": 1 } }, 
+    Tally.updateOne(
+        { date: dateObject, "moods.name": mood }, 
+        { $inc: { totalEntries: 1, "moods.$.tally": 1 } }, 
         {new: false}, 
         (err) => {
             if(err) return res.status(404).send();
             else return res.status(204).send();
     })
 }); 
+
+// get moods for previous dates
+router.get("/moods/previous", (req, res) => {
+    let { dateObject } = req;
+
+    Tally.find({ date: { $not: { $eq: dateObject } } }, (err, tallyDocumentList) => {
+        if(err)
+            return res.json({success: false, error: err, data: []});
+        
+        return res.send({success: true, data: tallyDocumentList});
+    });
+});
 
 app.listen(API_PORT, () => { console.log(`Listening on port ${API_PORT}.`)});
